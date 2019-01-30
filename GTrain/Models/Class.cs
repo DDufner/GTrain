1276c1f6408e4c743d37bccb62f6ddda7a609506
TheDommer <Dominick@DESKTOP-3I3RJJN>
2019-01-29 19:37:30 -0600

    repo3 test

---
+++ GTrain/Models/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GTrain.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public IList<TopicCategory> TopicCategories { get; set; } = new List<TopicCategory>();
+
+    }
+}
